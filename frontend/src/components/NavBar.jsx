a7c1381618dc676a0cce19359701dfae38124755
Haolong <chl123.chenhaolong@gmail.com>
2025-03-23 16:17:34 -0400

    fix: update notification button to reflect unread notifications

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -6,8 +6,10 @@ import debounce from 'lodash.debounce';
 import { DefaultAvatar } from './DefaultAvatar';
 import { Bell } from './Bell';
 import { BellWithRedDot } from './BellWithRedDot';
+import { useAuth } from '../context/AuthContext';
 
 export function NavBar() {
+  const { notifications } = useAuth();
   const navigate = useNavigate();
   const [theme, setTheme] = useState('light');
   const [search, setSearch] = useState({
@@ -256,7 +258,7 @@ export function NavBar() {
               d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
           </svg>
         </label>
-        <button className='hover:cursor-pointer notification-button' onClick={() => navigate('/notifications')}><BellWithRedDot className="w-6 h-6" /></button>
+        <button className='hover:cursor-pointer notification-button' onClick={() => navigate('/notifications')}>{notifications.length > 0 ? (<BellWithRedDot className="w-6 h-6" />) : (<Bell className="w-6 h-6" />)}</button>
         <Avatar size={32} />
         <div></div>
       </div>
